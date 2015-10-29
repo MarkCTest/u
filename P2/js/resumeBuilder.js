52fe8f968f97a1f10127abba5efea35ad6bd6135
MarkCTest <cyreath@gmail.com>
2015-10-29 13:22:35 +0000

    Adding P2 files

---
+++ P2/js/resumeBuilder.js
@@ -0,0 +1,83 @@
+var bio = {
+    "name": "Mark Crowther",
+    "role": "Software Tester",
+    "welcome": "Welcome to my Udacity pages, part of the Front-end Web Developer course. Totally Udacious!",
+    "contacts": {
+        "email": "mark@cyreath.co.uk",
+        "github": "MarkCTest",
+        "twitter": "@MarkCTest",
+        "location": "London"
+        },
+    "skills": ["HTML5", "CSS3", "JavaScript", "JQuery", "JSON", "Testing", "Automation"],
+    "biopic": "http://cyreath.co.uk/images/mark001.jpg",
+    "display": //function
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "The Open University",
+            "location": "Milton Keynes",
+            "degree": "Computing and Systems Practice",
+            "major": "Automation",
+            "dates": "2011",
+            "url": "http://www.open.ac.uk/"
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Tenable Certified Nessus Auditor",
+            "school": "Tenable",
+            "dates": "2014",
+            "url": "http://elearn.tenable.com/course/info.php?id=55"
+        },
+        {
+            "name": "Udacity",
+            "location": "Mountain View",
+            "degree": "Front-end Web Developer",
+            "major": "Web Development",
+            "dates": "2015",
+            "url": "https://www.udacity.com"
+        }        
+    ],
+    "display": //function
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Piccadilly Group",
+            "title": "Principal Consultant",
+            "location": "London, UK",
+            "dates": "April 2013 to present",
+            "description": "On-site consulting in Test Management and Delivery"
+        },
+        {
+            "employer": "Pride Systems Ltd",
+            "title": "Test Architect",
+            "location": "Malaga, Spain",
+            "dates": "May 2011 to March 2013",
+            "description": "Design and Deliver hands-on testing solutions"
+        }
+    ],
+    "display": //function
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "Automation Engineer",
+            "dates": "2014",
+            "description": "Test Automation of over 300 applications for an investment bank",
+            "images": "http://logodatabases.com/wp-content/uploads/2012/03/deutsche-bank.jpg"
+        },
+        {
+            "title": "Test Support Architect",
+            "dates": "2015",
+            "description": "Analysis and design of test environments",
+            "images": "http://www.barclaycard.co.uk/personal/images/barclaycard_logo.png"
+        }
+    ],
+    "display": //function        
+}
+    
