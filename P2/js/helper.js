bf312247df7db7a4f335344a31fcd10ac7624ba9
MarkCTest <cyreath@gmail.com>
2015-11-09 22:47:48 +0000

    Update to P2 files

--- P2/js/helper.js
+++ P2/js/helper.js
@@ -11,7 +11,7 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
--- P2/js/resumeBuilder.js
+++ P2/js/resumeBuilder.js
@@ -1,18 +1,38 @@
+
 var bio = {
     "name": "Mark Crowther",
-    "role": "Software Tester",
-    "welcome": "Welcome to my Udacity pages, part of the Front-end Web Developer course. Totally Udacious!",
+    "role": "Front-end Test Architect",
+    "welcome": "Welcome to my Udacity pages, part of the Front-end Web Developer course. Here you'll find a list of the web programming skills I'm developing as part of the Udacity course. Totally Udacious!",
     "contacts": {
+        "mobile": "+44 (0)7725 838417",
         "email": "mark@cyreath.co.uk",
         "github": "MarkCTest",
         "twitter": "@MarkCTest",
-        "location": "London"
+        "blog": "http://cyreath.blogspot.com/",
+        "location": "London, UK"
         },
     "skills": ["HTML5", "CSS3", "JavaScript", "JQuery", "JSON", "Testing", "Automation"],
-    "biopic": "http://cyreath.co.uk/images/mark001.jpg",
-    "display": //function
-}
+    "biopic": "http://cyreath.co.uk/dev/udacity/images/mark-crowther.png"
+};
 
+// Function to add skills if there are any in the skills array within the bio object
+bio.display = function() {
+    if (bio.skills === 0) {
+            console.log("Nothing in Skills so skills header is hidden");
+        } else {
+            var formattedSkillsStart = HTMLskillsStart;
+            $("#header").append(formattedSkillsStart);
+            
+// The solution at the end of [JavaScript Basics > Flow Control > Quiz] seemed very repetative, so I went for this instead
+            bio.skills.forEach(function(item) {
+                var formattedSkills = HTMLskills.replace("%data%",item);
+                $("#skills").append(formattedSkills);
+            });
+        }; //end Else
+}; // end displaySkills function
+
+
+ // ###########################      EDUCATION          ##############################
 var education = {
     "schools": [
         {
@@ -40,8 +60,10 @@ var education = {
             "url": "https://www.udacity.com"
         }        
     ],
-    "display": //function
+    "display": "place function here"    
 }
+
+ // ###########################      WORK          ##############################
 
 var work = {
     "jobs": [
@@ -60,7 +82,7 @@ var work = {
             "description": "Design and Deliver hands-on testing solutions"
         }
     ],
-    "display": //function
+    "display": "place function here"
 }
 
 var projects = {
@@ -78,6 +100,47 @@ var projects = {
             "images": "http://www.barclaycard.co.uk/personal/images/barclaycard_logo.png"
         }
     ],
-    "display": //function        
+    "display": "place function here"
 }
-    
+
+
+// TO DO: Convert all the Code for adding elements to the webpage into FUNCTIONS
+// #############################################################################
+
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    $("#header").prepend(formattedName);
+
+// TOP CONTACTS
+// ############# TODO: Change this to a forEach loop #############
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    $("#topContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    $("#topContacts").append(formattedEmail);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    $("#topContacts").append(formattedGithub);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    $("#topContacts").append(formattedTwitter);
+
+var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+    $("#topContacts").append(formattedBlog);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    $("#topContacts").append(formattedLocation);
+
+// Extra #Header Items
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    $("#header").append(formattedBioPic);
+
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcome);
+    $("#header").append(formattedWelcome);
+
+// Call the resumeBuilder Functions to build the resume content
+bio.display();
