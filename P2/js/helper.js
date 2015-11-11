6de1c27b9f5ca91016653f99d6d2916db2006e85
MarkCTest <cyreath@gmail.com>
2015-11-11 22:36:04 +0000

    Added click catcher

--- P2/js/helper.js
+++ P2/js/helper.js
@@ -14,7 +14,7 @@ var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span class="white-text">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLyoutube = '<li class="flex-item"><span class="orange-text">youtube</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
@@ -83,9 +83,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
-});
+    var x = loc.pageX;
+    var y = loc.pageY;
 
+    logClicks(x,y);
+});
 
 
 /*
