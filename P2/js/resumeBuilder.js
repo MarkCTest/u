d34011b71835f4ec7d8e5dfdbe9f7c4856bab285
MarkCTest <cyreath@gmail.com>
2015-11-17 23:20:24 +0000

    Updated file with reviewer feedback

--- P2/js/resumeBuilder.js
+++ P2/js/resumeBuilder.js
@@ -1,225 +1,205 @@
-
 var bio = {
     "name": "Mark Crowther",
     "role": "Front-end Test Architect",
-    "welcome": "Welcome to my Udacity pages, part of the Front-end Web Developer course. Here you'll find a list of the web programming skills I'm developing as part of the Udacity course. A Totally Udacious Mindblow&trade;!",
+    "welcome": "Welcome to my Udacity pages, part of the Front-end Web Developer course. " +
+        "Here you'll find a list of the web programming skills I'm developing as part of the Udacity course. " +
+        "A Totally Udacious Mindblow&trade;!",
     "contacts": {
         "youtube": "http://www.youtube.com/user/Cyreath",
         "email": "mark@cyreath.co.uk",
         "github": "MarkCTest",
         "twitter": "@MarkCTest",
-        "blog": "http://cyreath.blogspot.com/",
+        "mobile": "00011 222 334",
         "location": "London, UK"
-        },
+    },
     "skills": ["HTML5", "CSS3", "JavaScript", "JQuery", "JSON", "Test Automation"],
     "biopic": "images/mark001.jpg",
-    display : function() {
-        if (bio.skills === 0) {
-            console.log("Nothing in Skills so skills header is hidden");
-        } else {
-            var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-                $("#header").prepend(formattedRole);
+    display: function() {
+            if (bio.skills === 0) {
+                console.log("Nothing in Skills so skills header is hidden");
+            } else {
+                // added the variables below to try out the review suggestion, works well :)
+                var $head = $("#header");
+                var $top = $("#topContacts");
+                var $foot = $("#footerContacts");
+                var data = "%data%";
 
-            var formattedName = HTMLheaderName.replace("%data%", bio.name);
-                $("#header").prepend(formattedName);
+                var formattedRole = HTMLheaderRole.replace(data, bio.role);
+                var formattedName = HTMLheaderName.replace(data, bio.name);
 
-            console.log("Name and Role added to the resume");
+                $head.prepend(formattedName, formattedRole);
 
-            var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-                $("#topContacts").append(formattedEmail);
-            
-            var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-                $("#topContacts").append(formattedGithub);
-                $("#footerContacts").append(formattedGithub);
-            
-            var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-                $("#topContacts").append(formattedTwitter);
-                $("#footerContacts").append(formattedTwitter);
-            
-            var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
-                $("#topContacts").append(formattedBlog);
-                 $("#footerContacts").append(formattedBlog);
-            
-            var formattedYoutube = HTMLyoutube.replace("%data%", bio.contacts.youtube);
-                $("#topContacts").append(formattedYoutube);
-                $("#footerContacts").append(formattedYoutube);
-            
-            var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-                $("#topContacts").append(formattedLocation);
+                console.log("Name and Role added to the resume");
 
-            var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-                $("#header").append(formattedBioPic);
+                var formattedEmail = HTMLemail.replace(data, bio.contacts.email);
+                var formattedGithub = HTMLgithub.replace(data, bio.contacts.github);
+                var formattedTwitter = HTMLtwitter.replace(data, bio.contacts.twitter);
+                var formattedMobile = HTMLmobile.replace(data, bio.contacts.mobile);
+                var formattedYoutube = HTMLyoutube.replace(data, bio.contacts.youtube);
+                var formattedLocation = HTMLlocation.replace(data, bio.contacts.location);
 
-            var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcome);
-                $("#header").append(formattedWelcome);
-            
-            var formattedSkillsStart = HTMLskillsStart;
-                $("#header").append(formattedSkillsStart);
-            
-            // The solution at the end of [JavaScript Basics > Flow Control > Quiz] seemed very repetative, so I went for this instead
-            bio.skills.forEach(function(item) {
-                var formattedSkills = HTMLskills.replace("%data%", item);
-                $("#skills").append(formattedSkills);
-            });
-            console.log("Bio added to the resume");
-        }; //end Else
-    } // end bio.display function      
+                $top.append(formattedGithub, formattedEmail, formattedTwitter, formattedMobile, formattedYoutube, formattedLocation);
+                $foot.append(formattedGithub, formattedTwitter, formattedMobile, formattedYoutube);
+
+                var formattedBioPic = HTMLbioPic.replace(data, bio.biopic);
+                var formattedWelcome = HTMLwelcomeMsg.replace(data, bio.welcome);
+                var formattedSkillsStart = HTMLskillsStart;
+
+                $head.append(formattedBioPic, formattedWelcome, formattedSkillsStart);
+
+                // The solution at the end of [JavaScript Basics > Flow Control > Quiz] seemed very repetative, so I went for this instead
+                bio.skills.forEach(function(item) {
+                    var formattedSkills = HTMLskills.replace(data, item);
+                    $("#skills").append(formattedSkills);
+                });
+                console.log("Bio added to the resume");
+            } //end Else
+        } // end bio.display function      
 };
 
- // ###########################      EDUCATION          #############################
+// ###########################      EDUCATION          #############################
 var education = {
-    "schools": [
-        {
-            "name": "The Open University",
-            "location": "Milton Keynes",
-            "degree": "Computing and Systems Practice",
-            "major": "Test Automation",
-            "dates": "2011",
-            "url": "http://www.open.ac.uk/"
-        }
-    ],
-    "onlineCourses": [
-        {
-            "title": "Tenable Certified Nessus Auditor",
-            "school": "Tenable",
-            "dates": "2014",
-            "url": "http://elearn.tenable.com/course/info.php?id=55"
-        },
-        {
-            "title": "Front-end Web Developer",
-            "school": "Udacity",
-            "dates": "2015",
-            "url": "https://www.udacity.com"
-        }        
-    ],
-    
-display : function() {
-    $("#education").append(HTMLschoolStart);
-    
-    
-    for (eachSchool in education.schools) {
-        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[eachSchool].name);
-        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[eachSchool].degree);
-        var formattedSchoolCombined = formattedSchoolName + formattedSchoolDegree;
-        $(".education-entry:last").append(formattedSchoolCombined);      
+    "schools": [{
+        "name": "The Open University",
+        "location": "Milton Keynes",
+        "degree": "Computing and Systems Practice",
+        "major": "Test Automation",
+        "dates": "2011",
+        "url": "http://www.open.ac.uk/"
+    }],
+    "onlineCourses": [{
+        "title": "Tenable Certified Nessus Auditor",
+        "school": "Tenable",
+        "dates": "2014",
+        "url": "http://elearn.tenable.com/course/info.php?id=55"
+    }, {
+        "title": "Front-end Web Developer",
+        "school": "Udacity",
+        "dates": "2015",
+        "url": "https://www.udacity.com"
+    }],
 
-        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[eachSchool].dates);
-        $(".education-entry:last").append(formattedSchoolDates);
+    display: function() {
+        $("#education").append(HTMLschoolStart);
 
-        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[eachSchool].location);
-        $(".education-entry:last").append(formattedSchoolLocation);
+        var $eduLast = $(".education-entry:last");
 
-        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[eachSchool].major);
-        $(".education-entry:last").append(formattedSchoolMajor);
+        console.log("Education length is " + education.schools.length);
         
-        var formattedSchoolURL = HTMLschoolName.replace("%data%", education.schools[eachSchool].url);
-        $(".education-entry:last").append(formattedSchoolURL);
+        for (eachSchool in education.schools) {
+            var sIndex = education.schools[eachSchool];
+            var formattedSchoolName = HTMLschoolName.replace("%data%", sIndex.name);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", sIndex.degree);
+            var formattedSchoolCombined = formattedSchoolName + formattedSchoolDegree;
+
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", sIndex.dates);
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", sIndex.location);
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", sIndex.major);
+            var formattedSchoolURL = HTMLschoolName.replace("%data%", sIndex.url);
+
+            $eduLast.append(formattedSchoolCombined, formattedSchoolDates, formattedSchoolLocation, formattedSchoolMajor, formattedSchoolURL);
         }
 
-    $("#education").append(HTMLonlineClasses);
-    
-    for (eachOnlineClass in education.onlineCourses) {
-        $(HTMLschoolStart).insertAfter($("h3"));
-        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[eachOnlineClass].title);
-        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[eachOnlineClass].school);
-        var formattedOnlineCombined = formattedOnlineTitle + formattedOnlineSchool;
-        $(".education-entry:last").append(formattedOnlineCombined);
-        
-        var formattedOnlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[eachOnlineClass].dates);
-        $(".education-entry:last").append(formattedOnlineDate);
- 
-        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[eachOnlineClass].url);
-        $(".education-entry:last").append(formattedOnlineURL);        
-        
+        $("#education").append(HTMLonlineClasses);
+
+        for (eachOnlineClass in education.onlineCourses) {
+            $(HTMLschoolStart).insertAfter($("h3"));
+            var ocIndex = education.onlineCourses[eachOnlineClass];
+
+            var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", ocIndex.title);
+            var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", ocIndex.school);
+            var formattedOnlineCombined = formattedOnlineTitle + formattedOnlineSchool;
+            var formattedOnlineDate = HTMLonlineDates.replace("%data%", ocIndex.dates);
+            var formattedOnlineURL = HTMLonlineURL.replace("%data%", ocIndex.url);
+
+            $eduLast.append(formattedOnlineCombined, formattedOnlineDate, formattedOnlineURL);
+
         }
-        console.log("Schools added to the resume")
-    }      
+        console.log("Schools added to the resume");
+    }
 };
 
- // ###########################        WORK             #############################
+// ###########################        WORK             #############################
 var work = {
-    "jobs": [
-        {
-            "employer": "Piccadilly Group",
-            "title": "Principal Consultant",
-            "location": "London, UK",
-            "dates": "April 2013 to present",
-            "description": "In this current role I deliver on-site consulting in Test Management for finance and banking clients. This can involve analysis and planning to execution and reporting. I'm responsbile for coding automated unit and functional system tests using tools such as Selenium and NightWatch.js along with integrated dashbaords."
-        },
-        {
-            "employer": "Pride Systems Ltd",
-            "title": "Front-end Test Architect",
-            "location": "Malaga, Spain",
-            "dates": "May 2011 to March 2013",
-            "description": "In this role I was responsible for developing web based dashboards and reports to help display and visualise testing data. I was required to design and code HTML, CSS and JavaScript web solutions that could be used by the team and senior management alike."
-        }],
-display : function() {
-    for (eachJob in work.jobs) {
-        $("#workExperience").append(HTMLworkStart);
-     
-        var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[eachJob].employer);
-        var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[eachJob].title);
-        var concatEmployerAndTitle = formattedWorkEmployer + formattedWorkTitle;
-        $(".work-entry:last").append(concatEmployerAndTitle);
+    "jobs": [{
+        "employer": "Piccadilly Group",
+        "title": "Principal Consultant",
+        "location": "London, UK",
+        "dates": "April 2013 to present",
+        "description": "In this current role I deliver on-site consulting in Test Management for finance and banking clients." +
+            "This can involve analysis and planning to execution and reporting. I'm responsbile for coding automated unit and" +
+            "functional system tests using tools such as Selenium and NightWatch.js along with integrated dashboards."
+    }, {
+        "employer": "Pride Systems Ltd",
+        "title": "Front-end Test Architect",
+        "location": "Malaga, Spain",
+        "dates": "May 2011 to March 2013",
+        "description": "In this role I was responsible for developing web based dashboards and reports " +
+            "to help display and visualise testing data. I was required to design and code HTML, CSS and JavaScript " +
+            "web solutions that could be used by the team and senior management alike."
+    }],
+    display: function() {
+        for (eachJob in work.jobs) {
 
-        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[eachJob].location);
-        $(".work-entry:last").append(formattedLocation);        
-        
-        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[eachJob].dates);
-        $(".work-entry:last").append(formattedDates);
+            $("#workExperience").append(HTMLworkStart);
+            var wIndex = work.jobs[eachJob];
 
-        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[eachJob].description);
-        $(".work-entry:last").append(formattedDescription);        
+            var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", wIndex.employer);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", wIndex.title);
+            var concatEmployerAndTitle = formattedWorkEmployer + formattedWorkTitle;
+            var formattedLocation = HTMLworkLocation.replace("%data%", wIndex.location);
+            var formattedDates = HTMLworkDates.replace("%data%", wIndex.dates);
+            var formattedDescription = HTMLworkDescription.replace("%data%", wIndex.description);
 
-        console.log("Work items added to the resume");        
-        
+            $(".work-entry:last").append(concatEmployerAndTitle, formattedLocation, formattedDates, formattedDescription);
+
+            console.log("Work items added to the resume");
+
         }
-    }    
+    }
 };
 
 
 // ############################       PROJECTS          #############################
 var projects = {
-    "theProjects": [
-        {
-            "title": "Automation Engineer",
-            "dates": "2014",
-            "description": "Test Automation of over 300 applications for an investment bank. This involved tool selection based on the unique needs of the application team's testing problem, design of HTML based reporting interface, PoC build and deployment via a third party automation vendor.",
-            "images": ["images/logo-db.png", "images/icon-nightwatch.png", "images/logo-sel.png", "images/icon-html5.png", "images/icon-css3.png"]
-        },
-        {
-            "title": "Test Support Architect",
-            "dates": "2015",
-            "description": "Analysis and design of test environments",
-            "images": ["images/logo-bc.png", "images/logo-itil.png"]
-        }],
-    
-display : function() {
-    for (eachProject in projects.theProjects) {
-        $("#projects").append(HTMLprojectStart);
-        
-        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.theProjects[eachProject].title);
-        $(".project-entry:last").append(formattedProjectTitle);
-        
-        var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.theProjects[eachProject].dates);
-        $(".project-entry:last").append(formattedProjectDates);
-        
-        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.theProjects[eachProject].description);
-        $(".project-entry:last").append(formattedProjectDescription);
+    "theProjects": [{
+        "title": "Automation Engineer",
+        "dates": "2014",
+        "description": "Test Automation of over 300 applications for an investment bank. This involved" +
+            "tool selection based on the unique needs of the application team's testing problem," +
+            "design of HTML based reporting interface, PoC build and deployment via a third party automation vendor.",
+        "images": ["images/logo-db.png", "images/icon-nightwatch.png", "images/logo-sel.png", "images/icon-html5.png", "images/icon-css3.png"]
+    }, {
+        "title": "Test Support Architect",
+        "dates": "2015",
+        "description": "Analysis and design of test environments",
+        "images": ["images/logo-bc.png", "images/logo-itil.png"]
+    }],
 
-            if (projects.theProjects[eachProject].images.length === 0) {
+    display: function() {
+        for (eachProject in projects.theProjects) {
+            $("#projects").append(HTMLprojectStart);
+            var pIndex = projects.theProjects[eachProject];
+
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", pIndex.title);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", pIndex.dates);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", pIndex.description);
+
+            $(".project-entry:last").append(formattedProjectTitle, formattedProjectDates, formattedProjectDescription);
+
+            if (pIndex.images.length === 0) {
                 console.log("Nothing in Images so Images won't be shown");
             } else {
                 for (eachImage in projects.theProjects[eachProject].images) {
-                     var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.theProjects[eachProject].images[eachImage]);
+                    var formattedProjectImage = HTMLprojectImage.replace("%data%", pIndex.images[eachImage]);
                     $(".project-entry:last").append(formattedProjectImage);
                 } // end FOR
             } //end of ELSE
-        
-        console.log("Projects added to the resume");
-        
+
+            console.log("Projects added to the resume");
+
         } // end of FOR
-    }    
+    }
 }; // end of PROJECTS object
 
 // Call the resumeBuilder Functions to build the resume content
